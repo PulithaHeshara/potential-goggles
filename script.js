c1b8277488d6f87ffb6445a77416b8da5f828020
PulithaHeshara <141006598+PulithaHeshara@users.noreply.github.com>
2023-09-04 12:06:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelector('.nav-menu').addEventListener('click',()=>{
+    document.querySelector('.ul').classList.toggle('transform');
+    
+})
+document.querySelector('.close').addEventListener('click',()=>{
+    document.querySelector('.ul').classList.toggle('transform');
+    
+})
+
